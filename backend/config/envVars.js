55e65091f4f10af16925559a42091b952b1d2904
prathamm3786 <155967795+prathamm3786@users.noreply.github.com>
2025-04-04 17:23:26 +0530

    netflix-clone

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -5,5 +5,6 @@ export const ENV_VARS = {
   MONGO_URL: process.env.MONGO_URL,
   PORT: process.env.PORT || 5000,
   SECRET_KEY: process.env.SECRET_KEY,
+  NODE_ENV: process.env.NODE_ENV,
   TMDB_API_KEY: process.env.TMDB_API_KEY,
 };
